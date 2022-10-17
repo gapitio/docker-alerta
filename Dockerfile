92e7e20a211a4dd3e6567f84c26bab1769d4eed1
sbgap <sb@gapit.io>
2022-10-17 10:49:20 +0200

    feat: add environment var for muting init script

--- Dockerfile
+++ Dockerfile
@@ -134,6 +134,8 @@ USER 1001
 
 COPY docker-entrypoint.sh /usr/local/bin/
 
+ENV INIT_LOG false
+
 ENTRYPOINT ["docker-entrypoint.sh"]
 
 EXPOSE 8080 1717
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 JINJA2="import os, sys, jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ)+'\n')"
 
@@ -10,6 +9,10 @@ NGINX_CONF_FILE=/app/nginx.conf
 UWSGI_CONF_FILE=/app/uwsgi.ini
 SUPERVISORD_CONF_FILE=/app/supervisord.conf
 
+if [ $INIT_LOG = true ]; then set -ex; else set -e; fi
+
+if [ $INIT_LOG = true ]; then env | sort; fi
+
 ADMIN_USER=${ADMIN_USERS%%,*}
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-alerta}
 MAXAGE=${ADMIN_KEY_MAXAGE:-315360000}  # default=10 years
@@ -18,21 +21,21 @@ env | sort
 
 # Generate minimal server config, if not supplied
 if [ ! -f "${ALERTA_SVR_CONF_FILE}" ]; then
-  echo "# Create server configuration file."
+  if [ $INIT_LOG = true ]; then echo "# Create server configuration file."; fi
   export SECRET_KEY=${SECRET_KEY:-$(< /dev/urandom tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= | head -c 32)}
   python3 -c "${JINJA2}" < ${ALERTA_SVR_CONF_FILE}.j2 >${ALERTA_SVR_CONF_FILE}
 fi
 
 # Init admin users and API keys
 if [ -n "${ADMIN_USERS}" ]; then
-  echo "# Create admin users."
-  alertad user --all --password "${ADMIN_PASSWORD}" || true
-  echo "# Create admin API keys."
-  alertad key --all
+  if [ $INIT_LOG = true ]; then echo "# Create admin users."; fi
+  if [ $INIT_LOG = true ]; then alertad user --all --password "${ADMIN_PASSWORD}" || true; else alertad user --all --password "${ADMIN_PASSWORD}" > /dev/null || true; fi
+  if [ $INIT_LOG = true ]; then echo "# Create admin API keys."; fi
+  if [ $INIT_LOG = true ]; then alertad key --all else alertad key --all > /dev/null; fi
 
   # Create user-defined API key, if required
   if [ -n "${ADMIN_KEY}" ]; then
-    echo "# Create user-defined admin API key."
+    if [ $INIT_LOG = true ]; then echo "# Create user-defined admin API key."; fi
     alertad key --username "${ADMIN_USER}" --key "${ADMIN_KEY}" --duration "${MAXAGE}"
   fi
 fi
@@ -41,7 +44,7 @@ fi
 if [ ! -f "${ALERTA_CONF_FILE}" ]; then
   # Add API key to client config, if required
   if [ "${AUTH_REQUIRED,,}" == "true" ]; then
-    echo "# Auth enabled; add admin API key to client configuration."
+    if [ $INIT_LOG = true ]; then echo "# Auth enabled; add admin API key to client configuration."; fi
     HOUSEKEEPING_SCOPES="--scope read --scope write:alerts --scope admin:management"
     if grep -qE 'CUSTOMER_VIEWS.*=.*True' ${ALERTA_SVR_CONF_FILE};then
       HOUSEKEEPING_SCOPES="--scope admin:alerts ${HOUSEKEEPING_SCOPES}"
@@ -81,21 +84,22 @@ if [ ! -f "${ALERTA_WEB_CONF_FILE}" ]; then
   python3 -c "${JINJA2}" < ${ALERTA_WEB_CONF_FILE}.j2 >${ALERTA_WEB_CONF_FILE}
 fi
 
-echo
-echo '# Checking versions.'
-echo Alerta Server ${SERVER_VERSION}
-echo Alerta Client ${CLIENT_VERSION}
-echo Alerta WebUI  ${WEBUI_VERSION}
+if [ $INIT_LOG = true ]; then 
+  echo
+  echo '# Checking versions.'
+  echo Alerta Server ${SERVER_VERSION}
+  echo Alerta Client ${CLIENT_VERSION}
+  echo Alerta WebUI  ${WEBUI_VERSION}
 
-nginx -v
-echo uwsgi $(uwsgi --version)
-mongo --version | grep MongoDB
-psql --version
-python3 --version
-/venv/bin/pip list
+  nginx -v
+  echo uwsgi $(uwsgi --version)
+  mongo --version | grep MongoDB
+  psql --version
+  python3 --version
+  /venv/bin/pip list
 
-echo
-echo 'Alerta init process complete; ready for start up.'
-echo
-
+  echo
+  echo 'Alerta init process complete; ready for start up.'
+  echo
+fi
 exec "$@"
